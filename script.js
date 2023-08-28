8312698b03a38d032dfeb82c386e2a7a06e6d96a
Radosław Nowak <radeknowak2k19@gmail.com>
2023-08-28 13:39:11 +0200

    Created changeColor function

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 console.log("Hello from Nerdbord!");
 const boxElement = document.querySelector(".box");
-
-// #0000ff; #ff0000; #ffd700; #adff2f;
-
-const generateRandomColor = () => {
-	setInterval(() => {
-		const color = `#${Math.floor(Math.random() * 0xffffff)}`;
-		boxElement.style.borderColor = color;
-	}, 3000);
+let i = 0;
+const changeColor = () => {
+	const colors = ["#0000ff", "#ff0000", "#ffd700", "#adff2f"];
+	i = (i + 1) % colors.length;
+	console.log(i);
+	boxElement.style.borderColor = colors[i];
 };
-generateRandomColor();
+setInterval(changeColor, 3000);
