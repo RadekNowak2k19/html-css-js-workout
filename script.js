eee7aa259151c4e6a7cf1c8045ba67fb8e204d37
Radosław Nowak <radeknowak2k19@gmail.com>
2023-08-28 13:25:03 +0200

    Created generateRandomColor function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello from Nerdbord!')
+console.log("Hello from Nerdbord!");
+const boxElement = document.querySelector(".box");
+
+// #0000ff; #ff0000; #ffd700; #adff2f;
+
+const generateRandomColor = () => {
+	setInterval(() => {
+		const color = `#${Math.floor(Math.random() * 0xffffff)}`;
+		boxElement.style.borderColor = color;
+	}, 3000);
+};
+generateRandomColor();
